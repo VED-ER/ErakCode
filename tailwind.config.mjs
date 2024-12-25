94dcbf3451446f1ed82c8c839fcf37ec07838bc2
Vedran Erak <vedran.erak@snapguest.si>
2024-12-25 22:18:01 +0100

    Add google font (Poppins) and custom font (Neuropolitical)
    Added some imports aliases in ts config, added basic layout file

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,19 @@
+import defaultTheme from "tailwindcss/defaultTheme"
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+                neuropolitcal: ["Neuropolitical"],
+            },
+            colors: {
+                primary: "#0000EC",
+                background: "#000102",
+            },
+        },
+    },
+    plugins: [],
 }
