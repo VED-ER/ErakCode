408f8ef9c71587e6e5ab2f049b099f060c883255
Vedran Erak <vedran1.erak1@gmail.com>
2025-02-27 14:58:39 +0100

    add fade in animation on sections

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -18,6 +18,15 @@ export default {
                 "2xl": "1440px",
                 "3xl": "1536px",
             },
+            animation: {
+                fadeIn: "fadeIn 1s ease-out ",
+            },
+            keyframes: {
+                fadeIn: {
+                    "0%": { opacity: 0 },
+                    "100%": { opacity: 1 },
+                },
+            },
         },
     },
     plugins: [],
